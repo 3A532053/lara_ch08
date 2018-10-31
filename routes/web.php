194a15e731fc3d486530283aed33923d44527054
3A532053 <showhk61@gmail.com>
2018-10-31 15:20:59 +0800

    練習2 設定學生科目成績參數

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('student/{student_no}', function ($student_no) {
-    return "學號:".$student_no;
+Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
+    return "學號:".$student_no."的".$subject."成績";
 });
-Route::get('student/{student_no}/score', function ($student_no) {
-    return "學號:".$student_no."的所有成績";
-});
+
