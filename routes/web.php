19c7579fe8eddca4c747cbc2293717add8001f6e
3A532053 <showhk61@gmail.com>
2018-10-31 17:50:35 +0800

    練習10 修改路由'cool'，使之加入namespace路由'Cool'當中

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,7 @@ Route::group(['preflx'=>'student'],function(){
 
  });
 Route::get('/','HomeController@index');
-Route::get('cool', 'Cool\TestController@index');
+
+Route::group(['namespace'=>'Cool'],function(){
+    Route::get('cool', 'TestController@index');
+});
