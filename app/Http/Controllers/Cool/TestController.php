613a9a9e80ab48981f681f71741ce2c3c3d2247d
3A532053 <showhk61@gmail.com>
2018-10-31 17:41:32 +0800

    練習10 TestController內加入indexc函數

--- app/Http/Controllers/Cool/TestController.php
+++ app/Http/Controllers/Cool/TestController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requsets;
+
+use App\Http\Controllers\Controller;
+
 class TestController extends Controller
 {
-    //
+    public function index(){
+        return 'Cooler ';
+    }
 }
