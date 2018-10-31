eba2374123832d1170bb88153c98454e5d704204
3A532053 <showhk61@gmail.com>
2018-10-31 16:42:26 +0800

    練習8 HomeController內加入index函數

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
 
 class HomeController extends Controller
 {
     //
 }
+public function index(){
+    return view('welcome');
+}
+
+
