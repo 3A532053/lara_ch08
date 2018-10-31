610ff148dd786e6a445d400920c7e9488dde44fb
3A532053 <showhk61@gmail.com>
2018-10-31 17:29:51 +0800

    練習9 修改路由，使之成為路由群組'student'當中的路由，使之可串接StudentController內的getStudentData及getStudentScore函數

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,16 @@
 
 Route::pattern('student_no','s[0-9]{10}');
 
-Route::group(['prefix'=>'student'],function() {
+Route::group(['preflx'=>'student'],function(){
 
     Route::get('{student_no}',[
         'as'=>'student',
-        'uses'=>function($student_no) {
-            return '學號:' . $student_no;
-        }
+        'uses'=>'StudentController@getStudentData'
     ]);
 
     Route::get('{student_no}/score/{subject?}',[
-        'as'=>'student',
-        'uses'=>function($student_no,$subject = null) {
-            return '學號:' . $student_no . ' 的' . ((is_null($subject)) ? '所有科目 ' : $subject) . '成績';
-        }
-    ])->where(['subject' => '(chinese|english|math)']);
-});
-Route::get('/','HomeController@index'); 
+        'as'=>'student.score',
+        'uses'=>'StudentController@getStudentScore'
+    ])->where(['subject'=>'(chinese|english|math)']);
+
+ });
